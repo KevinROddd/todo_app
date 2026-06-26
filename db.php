16720977a431cee9fe5ffe450fdf1afa6a76c1ac
Danilo Manuel Platania <danilomanuel.platania@itismajo.school>
2026-06-26 10:13:48 +0200

    db.php

---
+++ db.php
@@ -0,0 +1,21 @@
+<?php
+$host = 'localhost';
+$dbname = 'todo_app';
+$username = 'root';
+$password = '';
+
+try {
+    $pdo = new PDO(
+        "mysql:host=$host;dbname=$dbname;charset=utf8",
+        $username,
+        $password
+    );
+
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+} catch (PDOException $e) {
+    http_response_code(500);
+    echo json_encode(['errore' => 'Connessione al database fallita']);
+    exit;
+}
+?>
